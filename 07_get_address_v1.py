e4ba982b4917aa3eef739bbda52cf26f445dfafc
amaya26 <165841780+amaya26@users.noreply.github.com>
2024-06-07 13:14:56 +1200

    Add files via upload

---
+++ 07_get_address_v1.py
@@ -0,0 +1,10 @@
+# initializing string
+while True:
+    address = input("What is your address? ")
+    number = any(map(str.isdigit, address))
+    string = any(map(str.isalpha, address))
+    if number == True and string == True:
+        print("Program continues. ")
+
+    else:
+        print("Please enter a valid address. ")
